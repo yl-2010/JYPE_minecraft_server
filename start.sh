671d99a12b2747c1d748a4c7c1c1999426408bc1
Yan Levin <yl-2010@outlook.com>
2026-09-25 20:00:04 -0700

    world sync 8:00:04 pm Pacific

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 JAVA25="/opt/homebrew/opt/openjdk@25/bin/java"
 RUNNER="./scripts/run_with_discord.py"
-SERVER_JAR="./Fabric Minecraft Server Launcher 26.2 0.19.3.jar"
+SERVER_JAR="./Fabric Minecraft Server Launcher 26.3 0.19.5.jar"
 
 if [[ -f "$RUNNER" ]]; then
   exec python3 "$RUNNER" "$JAVA25" -Dapple.awt.UIElement=true -Xms2G -Xmx4G -jar "$SERVER_JAR" nogui
